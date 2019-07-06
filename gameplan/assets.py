f6116797f3e7027d26bd76992bdc51315da24dc3
David Brodsky <davidbrodsky21@gmail.com>
2019-07-06 13:48:43 -0400

    Refactored CashSavings to have initial balance be a contribution. Added property calculating value through time, which handles compounding interest.

--- gameplan/assets.py
+++ gameplan/assets.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from gameplan.cashflows import CashFlow
 from gameplan.collections import Contributions
+from gameplan.contributions import Contribution
 
 
 class Asset():
@@ -19,3 +20,33 @@ class CashSavings(Asset):
         self.initial_balance = initial_balance
         self.annualized_interest_rate = annualized_interest_rate
         self.contributions = Contributions(contributions={})
+        init_contrib = Contribution(
+            contribution_label='initial_balance',
+            amount=initial_balance,
+            start_dt=pd.datetime.today(),
+            recurring=False
+        )
+        self.add_contribution(init_contrib)
+
+
+    def add_contribution(self, contribution, label=None, if_exists='error'):
+        label = label if label else contribution.name
+        self.contributions.add_object(contribution, label, if_exists)
+
+
+    @property
+    def value_through_time(self):
+        t = self.contributions.total
+        date_diffs = t.index.to_series().diff()
+        compound_factors = date_diffs.apply(
+            lambda x: np.e**(self.annualized_interest_rate * x.days / 365.25)
+            )
+
+        totals = []
+        total = 0
+        for x, r in zip(t.values, compound_factors.values):
+            r = r if pd.notnull(r) else 0
+            total = x + r * total
+            totals.append(total)
+
+        return pd.Series(data=totals, index=t.index, name='total_value')
