f4a5f0934ffd342ae0a6a422b0b5c8c0ef1732e3
David Brodsky <davidbrodsky21@gmail.com>
2019-07-21 15:00:27 -0400

    Adding Equities class w/ stock path simulation functionality.

--- gameplan/assets.py
+++ gameplan/assets.py
@@ -1,5 +1,8 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+import random
+
+import fecon236.prob.sim as fe_sim
 
 from gameplan.cashflows import CashFlow
 from gameplan.collections import Contributions
@@ -50,3 +53,91 @@ class CashSavings(Asset):
             totals.append(total)
 
         return pd.Series(data=totals, index=t.index, name='total_value')
+
+
+class Equity(Asset): # should be type Investment
+    DEFAULT_DATE_RANGE = pd.date_range(
+        start=pd.datetime.today().date(),
+        end=pd.datetime.today().date() + pd.DateOffset(years=10),
+        freq='B'
+    )
+    DEFAULT_MU = fe_sim.SPXmean
+    DEFAULT_SIGMA = fe_sim.SPXsigma
+
+    def __init__(self, ticker='SPY', init_value=1, mu=DEFAULT_MU,
+                 sigma=DEFAULT_SIGMA, date_range=DEFAULT_DATE_RANGE):
+        self.ticker=ticker
+        self.init_value = init_value
+        self.mu = mu
+        self.sigma = sigma
+        self.date_range = date_range
+
+
+    def _generate_returns(self):
+        return fe_sim.gmix2ret(N=len(self.date_range), mean=self.mu, sigma=self.sigma)
+
+
+    def _generate_price_path(self, label=None):
+        returns = self._generate_returns()
+        prices = self.init_value * pd.Series(index=self.date_range, data=returns, name=label).cumprod()
+
+        return prices
+
+
+    def _generate_price_paths(self, how_many, write_type=None):
+        """
+            write_type: str, default None
+                None - don't save paths to object
+                overwrite - overwrite whatever's in self._price_paths
+                append - add to whatever's in self._price_paths
+                error - raise ValueError when self._price_paths is not empty else write to it
+        """
+        paths = []
+        for x in range(how_many):
+            paths.append(self._generate_price_path())
+
+        # TO DO: This is probably unacceptably gross, so we should rewrite it
+        if write_type is None:
+            pass
+        elif write_type == 'overwrite':
+            self._price_paths = paths
+        elif write_type == 'append':
+            if hasattr(self, '_price_paths'):
+                self._price_paths.append(paths)
+            else:
+                self._price_paths = paths
+        elif write_type == 'error':
+            if hasattr(self, '_price_paths'):
+                raise ValueError("self._price_paths is not empty, if you want to overwrite or append, specify w/ write_type")
+            else:
+                self._price_paths = paths
+        else:
+            raise ValueError(f"{write_type} is not an acceptable value for write_type")
+
+        return paths
+
+
+    def plot_price_paths(self, max_plots=1, random_sample=True, **kwargs):
+        all_paths = (self._price_paths if hasattr(self, '_price_paths')
+                     else self._generate_price_paths(how_many=max_plots)
+                     )
+        paths_to_plt = (random.sample(all_paths, max_plots) if random_sample
+                        else all_paths[:max_plots]
+                        )
+        return pd.concat(paths_to_plt, axis=1).plot(**kwargs)
+
+
+    def plot_price_path_quantiles(self, min_n=200, quantiles=[0.25, 0.5, 0.75],
+                                  return_df=False, show_mean=False, **kwargs):
+        n_paths_to_gen = (min_n if not hasattr(self, '_price_paths')
+                          else min_n - len(self._price_paths)
+                          )
+        new_paths = self._generate_price_paths(how_many=n_paths_to_gen)
+        all_paths = (self._price_path.append(new_paths) if hasattr(self, '_price_paths')
+                     else new_paths
+                     )
+        all_paths_df = pd.concat(all_paths, axis=1)
+        quantiles = all_paths_df.quantile(q=quantiles, axis=1).T
+        if show_mean: quantiles['mean'] = all_paths_df.mean(axis=1)
+        quantiles.plot(**kwargs)
+        if return_df: return quantiles
