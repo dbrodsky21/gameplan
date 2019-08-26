0769344c477d6da7b593f2546d83f603b8edf150
David Brodsky <davidbrodsky21@gmail.com>
2019-08-26 15:44:04 -0400

    Adding deductions/taxation functionality to salary class. Ultimately gets to a take_home_pay property. Deductions such as a 401k deduction should, in a different class be made to interact w/ investments/assets.

--- gameplan/income_streams.py
+++ gameplan/income_streams.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 
 from gameplan.cashflows import CashFlow
+from gameplan.collections import CashFlowCollection
+from gameplan.contributions import Deduction
 
 
 class IncomeStream(CashFlow):
@@ -22,7 +24,7 @@ class IncomeStream(CashFlow):
 
 class Salary(IncomeStream):
     def __init__(self, paycheck_amt, payday_freq, next_paycheck_dt=None,
-                 last_paycheck_dt=None):
+                 last_paycheck_dt=None, tax_rate=0.0):
 
         start_dt = (
             next_paycheck_dt if next_paycheck_dt
@@ -33,17 +35,85 @@ class Salary(IncomeStream):
             last_paycheck_dt if last_paycheck_dt
             else start_dt + pd.DateOffset(years=1)
         )
-
         super().__init__(
             income_type='salary',
             amount=paycheck_amt,
             freq=payday_freq,
             start_dt=start_dt,
-            end_dt=end_dt
+            end_dt=end_dt,
+            tax_rate=tax_rate
+        )
+        self.deductions = CashFlowCollection(
+            objects={},
+            totals_col_label='total_deductions'
+            )
+
+
+    def _create_deduction(self, label, amt=None, pct=None):
+        """
+        Note that there's a time component here as well, where amt/pct should
+        maybe be a timeseries.
+        """
+        if all([amt, pct]) or not any([amt, pct]):
+            # Note that (pct or amt == 0 will read as a False)
+            raise ValueError("Exactly one of [amt, pct] must not be None")
+        contrib_amt = amt if amt else self.amount * pct
+        deduction = Deduction(
+            deduction_label=label,
+            date_range=self.date_range,
+            values=[contrib_amt] * len(self.date_range),
         )
 
+        return deduction
+
+
+    def add_deduction(self, deduction=None, label=None, amt=None, pct=None,
+                      if_exists='error'):
+
+        if not deduction:
+            deduction = self._create_deduction(label, amt, pct)
+
+        label = label if label else deduction.name
+        self.deductions.add_object(deduction, label, if_exists)
+
+    @property
+    def total_deductions(self):
+        return pd.Series(self.deductions.total, name='total_deductions')
+
+
+    @property
+    def post_deductions(self):
+        # deductions are a negative value
+        post_deductions = self.cash_flows_df['salary'] + self.total_deductions
+        return pd.Series(post_deductions, name='salary_post_deductions')
+
+    @property
+    def total_taxes(self):
+        total_taxes = self.post_deductions * self.tax_rate
+        return pd.Series(total_taxes, name='total_taxes')
+
+
+    @property
+    def take_home_salary(self):
+        post_taxes = self.post_deductions - self.total_taxes
+        return pd.Series(post_taxes, name='take_home_salary')
+
+    @property
+    def paycheck_df(self):
+        df = pd.concat([
+                self.cash_flows_df['salary'],
+                self.total_deductions,
+                -self.total_taxes, # taxes should be a negative cashflow
+                self.take_home_salary
+            ], axis=1)
+        return df
 
     @property
     def annualized_salary(self):
         """TO DO: refactor"""
         return self.cash_flows_df.resample('365D').sum().values[0][0]
+
+
+class IncomeStreams(CashFlowCollection):
+    def __init__(self, income_streams={}):
+        super().__init__(collection_type=IncomeStream, objects=income_streams)
