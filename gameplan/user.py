f0795772ff3f42c870de098eda9998d468703cf1
David Brodsky <davidbrodsky21@gmail.com>
2019-12-01 14:53:07 -0500

    Adding initial functionality to get growth curves for salary based on user characteristics.

--- gameplan/user.py
+++ gameplan/user.py
@@ -2,24 +2,41 @@ import pandas as pd
 import numpy as np
 from typing import Callable, List, Optional, Union, Tuple
 
+from gameplan.growth.growth_models import KitcesIncomeGrowthModel
+
 class User():
     def __init__(self,
                  email: str,
                  birthday: Optional[pd.datetime] = None,
                  zip_code: Optional[int] = None,
-                 gender: Optional[str] = None
+                 gender: Optional[str] = None,
+                 income_percentile: int = 50
                  ) -> None:
         self.email = email.lower()
         self.birthday = birthday
         self.zip_code = zip_code
         self.gender = gender
+        self._income_percentile = income_percentile
 
     @property
     def age(self):
         return pd.datetime.today() - self.age
 
-    def get_income_percentile(self, source=None):
-        pass
+    @property
+    def income_percentile(self):
+        return self._income_percentile
 
-    def get_growth_curve(self, object_type, model):
-        pass
+    @income_percentile.setter
+    def income_percentile(self, val):
+        # TO DO: implement a get_income_percentile method to estimate
+        # this based on user demographic and other info
+        self._income_percentile = val
+
+    def get_growth_points_to_fit(self,
+                                 growth_model: KitcesIncomeGrowthModel,
+                                 start_dt: pd.datetime = pd.datetime.today(),
+                                 **kwargs
+                                 ) -> None:
+        gm = growth_model(user_birthday=self.birthday,
+                          income_percentile=self.income_percentile)
+        return gm.get_growth_points_to_fit(start_dt=start_dt)
