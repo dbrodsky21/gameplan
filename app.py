5b8ca14322d8a6d1d5ce1a78443309bf4130dd0a
David Brodsky <davidbrodsky21@gmail.com>
2019-12-25 23:08:52 -0500

    Changed graph to CDF.

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
-# import os
-
-import pandas_datareader.data as web
-import datetime
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 
-# import sys
-# print("cwd: " + os.getcwd())
-# print("sys.path: ")
-# print(sys.path)
-# sys.path.append(os.getcwd()+'/gameplan')
-# print(sys.path)
-# sys.path.append('/app/gameplan')
-# print("sys.path:")
-# print(sys.path)
+import plotly.express as px
+import plotly.graph_objects as go
+import scipy.stats as sp
 
 from gameplan.growth.income_percentile_estimate import get_working_population_data
 
@@ -45,34 +35,50 @@ app.layout = html.Div(children=[
         options=[{'label': dma, 'value': dma} for dma in DMAS],
         value='New York-Northern New Jersey-Long Island, NY-NJ-PA'
     ),
-    html.Div(dcc.Input(id='input-box', type='number')),
+    html.Div(dcc.Input(id='input-box', type='number', value=0)),
     # html.Button('Submit', id='button'),
     dcc.Graph(id='output-graph'),
 ])
 
-def get_fig():
-    METRO_AREA = 'New York'
-    YEARLY_SALARY = 180000
-    SEX = 'Male'
-
-
-    subset = cohort_data.query("AGE.between(27, 31) & metarea.str.contains(@METRO_AREA)")
+def get_fig(df, salary, dma, age_range=None, gender=None):
+    query = "metarea.str.contains(@dma) "
+    subset = df.query(query)
     subset['weight'] = subset.ASECWT / subset.ASECWT.sum()
     dist = subset.sort_values('inctot').loc[:, ['inctot', 'weight']]
     dist['cdf'] = dist.weight.cumsum()
-
-    fig = px.line(dist, x='inctot', y='cdf', range_x=(0, 250000), range_y=(0,1))
-    percentile = sp.percentileofscore(dist.inctot, YEARLY_SALARY)
-
+    fig = px.line(
+        dist,
+        x='inctot',
+        y='cdf',
+        range_x=(0, 250000),
+        range_y=(0,1)
+        )
+    percentile = sp.percentileofscore(dist.inctot, int(salary))
     fig.add_scatter(
-        x=[YEARLY_SALARY, YEARLY_SALARY],
+        x=[salary, salary],
         y=[0, 1],
-
         mode='lines',
         line=dict(color='black', width=2, dash='dot'),
         showlegend=False
     )
-    fig.add_annotation(text=f'${YEARLY_SALARY:,} = {percentile:.0f}th Percentile', x=YEARLY_SALARY, y=percentile/100, showarrow=True, align='left')
+    fig.add_annotation(
+        text=f'${salary:,} = {percentile:.0f}th Percentile',
+        x=salary,
+        y=percentile/100,
+        showarrow=True,
+        align='left'
+        )
+    layout = go.Layout(
+        title=f"Total Income Distribution by Metro Area",
+        yaxis={
+            "title": "% of Full-Time Workers Earning Up To $X Per Year",
+            "range": [0, 1],
+            },
+        xaxis={"title": "Total Annual Income"},
+        )
+    fig.update_layout(layout)
+
+    return fig
 
 
 @app.callback(
@@ -84,28 +90,8 @@ def get_fig():
         # Input(component_id='salary', component_property='date')
 )
 def update_figure(dma, salary):
-    df = working_pop.query("metarea == @dma")
-    return {
-            'data': [
-                {
-                    'x': [salary, salary],
-                    'y': [0, 1],
-                    'type': 'line',
-                    'name': dma
-                },
-                {
-                    'x': df.inctot,
-                    # 'y': [80, 180],
-                    'type': 'histogram',
-                    # 'histnorm': 'probability'
-                    # 'name': u'dropdown'
-                    },
-            ],
-            'layout': {
-                'title': dma,
-
-            }
-        }
+    fig = get_fig(df=working_pop, dma=dma, salary=salary)
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
