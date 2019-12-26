aad431428e2e8044a9eae6dc542f2a4b9bdf0a5b
David Brodsky <davidbrodsky21@gmail.com>
2019-12-26 15:23:51 -0500

    Added age & gender to app; updated formatting.

--- app.py
+++ app.py
@@ -27,21 +27,72 @@ DMAS = [
 ]
 
 app.layout = html.Div(children=[
-    html.Div(children='''
-        Metro Area (DMA):
-    '''),
-    dcc.Dropdown(
-        id='dma',
-        options=[{'label': dma, 'value': dma} for dma in DMAS],
-        value='New York-Northern New Jersey-Long Island, NY-NJ-PA'
-    ),
-    html.Div(dcc.Input(id='input-box', type='number', value=0)),
-    # html.Button('Submit', id='button'),
+    html.Div(children=[
+        'Metro Area (DMA):',
+        dcc.Dropdown(
+            id='dma',
+            options=[{'label': dma, 'value': dma} for dma in DMAS],
+            value='New York-Northern New Jersey-Long Island, NY-NJ-PA'
+        )]),
+    html.Div(children=[
+        'Total Annual Income: $',
+        dcc.Input(id='annual_income', type='number', value=30000)
+        ]),
+    html.Div(children=[
+        'Your Age: ',
+        dcc.Input(id='age_input', type='number', value=25),
+        html.Div(children=[
+            'Relevant Age Range for Cohort: ',
+            # dcc.Input(id='age_bucket_left', type='number', value=23),
+            # dcc.Input(id='age_bucket_right', type='number', value=27),
+            dcc.RangeSlider(
+                id='age_range',
+                count=1,
+                min=22,
+                max=65,
+                step=1,
+                value=[23, 27],
+                # marks=
+                tooltip={'always_visible': True, 'placement': 'bottom'}
+            )]),
+        ]),
+    html.Div(children=[
+        'Gender: ',
+        dcc.RadioItems(
+            id='gender',
+            options=[
+                {'label': 'Female', 'value': 'Female'},
+                {'label': 'Male', 'value': 'Male'},
+                {'label': 'Prefer Not To Say', 'value': 'Neither'}
+            ],
+            value='Male',
+            labelStyle={'display': 'inline-block'}
+        )
+        ]),
     dcc.Graph(id='output-graph'),
 ])
 
-def get_fig(df, salary, dma, age_range=None, gender=None):
-    query = "metarea.str.contains(@dma) "
+@app.callback(
+    Output('age_range', 'value'),
+    [Input('age_input', 'value')]
+    )
+def set_age_buckets(age_input):
+    return [age_input - 2, age_input + 2]
+
+def construct_query(dma, age_range, gender):
+    if gender == 'Neither':
+        gender_clause = "(sex == 'Male' | sex == 'Female')"
+    else:
+        gender_clause = f"sex == '{gender}'"
+    query = f" \
+        metarea.str.contains(@dma) \
+        & AGE.between(@age_range[0], @age_range[1]) \
+        & {gender_clause} \
+        "
+    return query
+
+def get_fig(df, salary, dma, age_range, gender):
+    query = construct_query(dma, age_range, gender)
     subset = df.query(query)
     subset['weight'] = subset.ASECWT / subset.ASECWT.sum()
     dist = subset.sort_values('inctot').loc[:, ['inctot', 'weight']]
@@ -66,17 +117,37 @@ def get_fig(df, salary, dma, age_range=None, gender=None):
         x=salary,
         y=percentile/100,
         showarrow=True,
-        align='left'
+        align='left',
+        font=dict(size=14)
         )
+    gender_clause = '' if gender == 'Neither' else f" {gender.lower()}"
+    title = (
+        f"Total income distribution among full-time{gender_clause} workers, "
+        f"aged {age_range[0]} to {age_range[1]}, "
+        f"living in {dma}"
+    )
     layout = go.Layout(
-        title=f"Total Income Distribution in {dma}",
+        title=title,
         yaxis={
             "title": "% of Full-Time Workers Earning Up To $X Per Year",
             "range": [0, 1],
             },
         xaxis={"title": "Total Annual Income"},
+        font=dict(
+            # family="Courier New, monospace",
+            size=10,
+            # color="#7f7f7f"
+        )
         )
     fig.update_layout(layout)
+    fig.add_annotation(
+        showarrow=False,
+        text=f'Sample includes {len(subset):,.0f} individuals.',
+        x=1,
+        y=-0.1,
+        xref='paper', yref='paper',
+        xanchor='right', yanchor='auto', xshift=0, yshift=0,
+    )
 
     return fig
 
@@ -85,12 +156,19 @@ def get_fig(df, salary, dma, age_range=None, gender=None):
     Output(component_id='output-graph', component_property='figure'),
     [
         Input(component_id='dma', component_property='value'),
-        Input('input-box', 'value')
+        Input('annual_income', 'value'),
+        Input('age_range', 'value'),
+        Input('gender', 'value'),
     ],
-        # Input(component_id='salary', component_property='date')
 )
-def update_figure(dma, salary):
-    fig = get_fig(df=working_pop, dma=dma, salary=salary)
+def update_figure(dma, salary, age_range, gender):
+    fig = get_fig(
+        df=working_pop,
+        dma=dma,
+        salary=salary,
+        age_range=age_range,
+        gender=gender
+        )
     return fig
 
 if __name__ == '__main__':
