88f93ea242e451203a4e58af9dc460b8dcc32f10
David Brodsky <davidbrodsky21@gmail.com>
2020-05-05 07:28:08 -0400

    Honestly don't remember what was going on here, in the middle of a major refactor, dropped it.

--- apps/forms.py
+++ apps/forms.py
@@ -5,6 +5,15 @@ import dash_bootstrap_components as dbc
 from datetime import datetime as dt
 import numpy as np
 import pandas as pd
+import plotly.express as px
+import scipy.stats as sp
+
+from gameplan.assets import Equity
+from gameplan.expenses import Expenses, Rent, MiscellaneousExpenses
+from gameplan.user import User
+from gameplan.income_streams import Salary
+from gameplan.portfolio import Portfolio
+from gameplan.growth.growth_models import KitcesIncomeGrowthModel
 
 # from gameplan.growth.income_percentile_estimate import get_working_population_data
 # from apps.income_forecast import get_income_dist_fig, get_income_trajectory_fig
@@ -55,8 +64,8 @@ dob_input = dbc.Col(
             html.Div(
                 dcc.DatePickerSingle(
                     id="dob_input",
-                    placeholder="MM/DD/YYYY",
-                    # date=dt(1985, 1, 1),
+                    # placeholder="MM/DD/YYYY",
+                    date=dt(1985, 1, 1),
                     initial_visible_month=dt(1985, 1, 1),
                     # with_portal=True,
                     # number_of_months_shown=1,
@@ -71,6 +80,7 @@ dob_input = dbc.Col(
         ],
     )
 )
+
 age_cohort_input = dbc.Col(
     dbc.FormGroup(
         [
@@ -168,7 +178,7 @@ salary_input = dbc.Col(
                         id="salary_input",
                         placeholder="Pre-tax income",
                         type="number",
-                        value=0,
+                        value=10000,
                         min=0,
                         step=1000,
                     ),
@@ -233,6 +243,34 @@ non_401k_investments_input = dbc.Col(
             ),
             dbc.FormText(
                 "What's the current approximate value of your investments outside your 401k / Roth IRA, if any?",
+                color="secondary",
+            ),
+        ],
+    )
+)
+
+future_non_401k_investments_input = dbc.Col(
+    dbc.FormGroup(
+        [
+            dbc.Label("Future Investments (Non-401k)", html_for="future_non_401k_investments_group"),
+            dbc.InputGroup(
+                [
+                    dbc.Input(
+                        id="future_non_401k_investments_input",
+                        # placeholder="Enter your existing cash savings",
+                        type="number",
+                        value=0,
+                        min=0,
+                        max=100,
+                        step=1,
+                    ),
+                    dbc.InputGroupAddon("%", addon_type="append"),
+                ],
+                id='future_non_401k_investments_group',
+                className="mb-3",
+            ),
+            dbc.FormText(
+                "Outside of your 401k, what proportion of your salary do you plan on investing on an ongoing basis?",
                 color="secondary",
             ),
         ],
@@ -408,6 +446,55 @@ non_housing_expense_input = dbc.Col(
     )
 )
 
+income_percentile_for_growth_input = dbc.Col(
+    dbc.FormGroup(
+        [
+            dbc.Label("Income Percentile For Growth Scenarios", html_for="income_percentile_for_growth_group"),
+            html.Div(
+                dcc.Slider(
+                    id='income_percentile_for_growth_input',
+                    # count=1,
+                    min=10,
+                    max=90,
+                    step=10,
+                    value=50,
+                    marks={
+                        10: "10",
+                        20: "20",
+                        30: "30",
+                        40: "40",
+                        50: "50",
+                        60: "60",
+                        70: "70",
+                        80: "80",
+                        90: "90",
+                    },
+                    tooltip={'placement': 'bottom'}
+                ),
+                className="dash-bootstrap"
+            ),
+            dbc.FormText(
+                (
+                    f"This is for growth scenarios "
+                ),
+                color="secondary",
+            ),
+        ],
+    )
+)
+
+"""
+## Output
+"""
+income_percentile_text = dbc.Col(
+    dbc.FormGroup(
+        html.Div([
+            dcc.Markdown(f"Among members of your cohort (people similar in age and location) you're currently in the:"),
+            dcc.Markdown(f"___ Percentile in terms of total annual income", id='percentile_text', style={'textAlign':'center', "font-size": 20}),
+            # dcc.Markdown(f"in terms of total annual income")
+        ]),
+    )
+)
 
 """
 ##  Graphs
@@ -415,6 +502,17 @@ non_housing_expense_input = dbc.Col(
 income_trajectory_graph = dcc.Loading(
     id="loading-income-trajectory",
     children=[dcc.Graph(id="forms-income-trajectory-graph")],
+    type="default",
+)
+expenses_trajectory_graph = dcc.Loading(
+    id="loading-expenses-trajectory",
+    children=[dcc.Graph(id="forms-expenses-trajectory-graph")],
+    type="default",
+)
+
+savings_graph = dcc.Loading(
+    id="loading-savings",
+    children=[dcc.Graph(id="forms-savings-graph")],
     type="default",
 )
 
@@ -446,8 +544,28 @@ layout = dbc.Container(
                 dbc.CardHeader('Salary Info'),
                 dbc.CardBody(
                     [
-                        dbc.Row(salary_input),
+                        dbc.Row([
+                            salary_input,
+                            income_percentile_text,
+                            income_percentile_for_growth_input
+                        ]),
                         income_trajectory_graph
+                    ]
+                )
+            ]
+            , color="light"
+            , style={'marginBottom': 20}
+        ),
+        dbc.Card(id='expenses_card', children=
+            [
+                dbc.CardHeader('Expenses'),
+                dbc.CardBody(
+                    [
+                        dbc.Row([
+                            housing_expense_input,
+                            non_housing_expense_input
+                        ]),
+                        expenses_trajectory_graph,
                     ]
                 )
             ]
@@ -460,6 +578,8 @@ layout = dbc.Container(
                 dbc.CardBody(
                     [
                         dbc.Row([cash_savings_input, non_401k_investments_input]),
+                        dbc.Row(future_non_401k_investments_input),
+                        savings_graph,
                         dbc.Row([existing_401k_input, ongoing_401k_contribution_input]),
                         dbc.Row([employer_401k_contribution_pct_input, employer_401k_contribution_rate_input]),
                     ]
@@ -468,23 +588,74 @@ layout = dbc.Container(
             , color="light"
             , style={'marginBottom': 20}
         ),
-        dbc.Card(id='expenses_card', children=
-            [
-                dbc.CardHeader('Expenses'),
-                dbc.CardBody(
-                    [
-                        dbc.Row([housing_expense_input, non_housing_expense_input]),
-                    ]
-                )
-            ]
-            , color="light"
-            , style={'marginBottom': 20}
-        ),
+
 
         # Hidden div inside the app that stores the intermediate value
         html.Div(id='cohort-df', style={'display': 'none'})
     ]
 )
+
+"""
+## Helper Functions
+"""
+
+def create_portfolio(email, dob, dma, inc_pctile, salary, housing_exp,
+                     non_housing_exp, initial_savings, current_investments,
+                     existing_401k_val, ongoing_401k_contrib_pct,
+                     employer_401k_pct, employer_401k_rate,):
+    user = User(
+        email=email,
+        birthday=pd.datetime.fromisoformat(dob),
+        zip_code=dma,
+        income_percentile=inc_pctile
+    )
+    sal_grwth_points = user.get_growth_points_to_fit(
+        growth_model=KitcesIncomeGrowthModel,
+        start_dt=pd.datetime.today()
+    )
+
+    s = Salary(
+        salary,
+        payday_freq='Y',
+        growth_points_to_fit=sal_grwth_points,
+        last_paycheck_dt= user.birthday + pd.DateOffset(years=65),
+        tax_rate=.35
+    )
+
+    port = Portfolio(initial_cash_savings=initial_savings, annualized_interest_rate=0.005)
+    port.add_income_stream(s, label='salary', if_exists='overwrite')
+
+    retirement_dt = pd.to_datetime(dob) + pd.DateOffset(years=65)
+    port.add_expense(
+        Rent(housing_exp, end_dt=retirement_dt),
+        label='housing expenses',
+        if_exists='overwrite'
+    )
+    port.add_expense(
+        MiscellaneousExpenses(non_housing_exp, end_dt=retirement_dt),
+        label='non-housing expenses',
+        if_exists='overwrite'
+    )
+
+    investments = Equity(init_value=current_investments)
+    port.add_asset(investments, label='investments', if_exists='overwrite')
+
+    # future_investments = Equity(init_value=current_investments)
+    # port.add_asset(future_investments, label='future_investments', if_exists='overwrite')
+
+    # if existing_401k_val >= 0:
+    existing_401k = Equity(init_value=existing_401k_val)
+    port.add_asset(existing_401k, label='401k', if_exists='overwrite')
+
+    if any([ongoing_401k_contrib_pct, employer_401k_pct]):
+        port.add_401k_contribution(
+            income_stream_label='salary',
+            contrib_pct=ongoing_401k_contrib_pct,
+            employer_match=dict(upto=employer_401k_pct, pct_match=employer_401k_rate),
+            label='401k'
+        )
+
+    return port
 
 """
 #  Callbacks
@@ -498,7 +669,6 @@ def set_age_buckets(dob):
     age_years =  np.floor(age_days / 365)
     return [age_years - 2, age_years + 2]
 
-
 @app.callback(
     Output('cohort-df', 'children'),
     [
@@ -509,7 +679,6 @@ def set_age_buckets(dob):
     ]
     )
 def get_cohort_df(dma, salary, age_range, gender):
-     # some expensive clean data step
      cleaned_df = inc.get_cohort_subset(
         df=working_pop,
         dma=dma,
@@ -517,55 +686,96 @@ def get_cohort_df(dma, salary, age_range, gender):
         gender=gender
         )
 
-     # more generally, this line would be
-     # json.dumps(cleaned_df)
      return cleaned_df.to_json(date_format='iso', orient='split')
-
 
 @app.callback(
     Output(component_id='forms-income-trajectory-graph', component_property='figure'),
     [
-        Input(component_id='cohort-df', component_property='children'),
+        # Input(component_id='cohort-df', component_property='children'),
         Input('salary_input', 'value'),
         Input('dob_input', 'date'),
+        Input('income_percentile_for_growth_input', 'value'),
     ],
 )
-def update_income_trajectory_figure(cohort_json, salary, dob):
-    cohort_df = pd.read_json(cohort_json, orient='split')
+def update_income_trajectory_figure(salary, dob, percentile):
+    # cohort_df = pd.read_json(cohort_json, orient='split')
     age_days = (pd.datetime.today() - pd.to_datetime(dob)).days
     age_years =  np.floor(age_days / 365)
     fig = inc.get_income_trajectory_fig(
-        cohort_df=cohort_df,
         salary=salary,
         age=age_years,
+        income_percentile=percentile
         )
     return fig
 
+@app.callback(
+    [
+        Output('percentile_text', 'children'),
+        Output('income_percentile_for_growth_input', 'value'),
+    ],
+    [
+        Input('cohort-df', 'children'),
+        Input('salary_input', 'value'),
+    ]
+)
+def update_percentile_text(cohort_json, salary):
+    cohort_df = pd.read_json(cohort_json, orient='split')
+    perc = sp.percentileofscore(cohort_df.inctot, salary)
+    for_text = f"**{inc.get_percentile_label(perc)} percentile** in terms of total annual income"
+    return for_text, inc.get_pctile_for_growth(perc)
 
-# 1. Create a user object
-# 2. Get user's income percentile
-# 3. Create a salary object using growth points from user
-# 4. Create a portfolio
-#     - init w/ initial savings (call back)
-#     - init w/ interest rate (assumed)
-# 5. Add salary to portfolio
-# 6. Add 401k (initialized w/ existing 401k)
-# 7. Add 401k contributions
-# 8. Add non-401k investments?
-# 9. Create rent expense
-# 10. Create Misc expense
+@app.callback(
+    Output(component_id='forms-expenses-trajectory-graph', component_property='figure'),
+    [
+        # Input(component_id='cohort-df', component_property='children'),
+        Input('housing_expense_input', 'value'),
+        Input('non_housing_expense_input', 'value'),
+        Input('dob_input', 'date'),
+    ],
+)
+def update_expenses_trajectory_figure(housing_expense, non_housing_expense, dob):
+    # cohort_df = pd.read_json(cohort_json, orient='split')
+    age_days = (pd.datetime.today() - pd.to_datetime(dob)).days
+    age_years =  np.floor(age_days / 365)
+    retirement_dt = pd.to_datetime(dob) + pd.DateOffset(years=65)
+    exp = Expenses(expenses={
+        'housing': Rent(housing_expense, end_dt=retirement_dt),
+        'non-housing': MiscellaneousExpenses(non_housing_expense, end_dt=retirement_dt)
+    })
+    fig = px.line((-1*exp.as_df).stack().reset_index(), x='level_0', y=0, color='level_1')
+    return fig
 
-# from gameplan.user import User
-#
-# subset_df =
-#
-# user = User(
-#     email=callback_email,
-#     birthday=callback_email,
-#     zip_code=geo,
-#     gender=callback_gender,
-#     # percentile=calculated_percentile
-# )
-# inc.get_income_percentile():
-# def get_income_percentile()
-# portfolio =
+
+@app.callback(
+    Output('forms-savings-graph', 'figure'),
+    [
+        Input('email_input', 'value'),
+        Input('dob_input', 'date'),
+        Input('geo_input', 'value'),
+        Input('income_percentile_for_growth_input', 'value'),
+        Input('salary_input', 'value'),
+        Input('housing_expense_input', 'value'),
+        Input('non_housing_expense_input', 'value'),
+        Input('cash_savings_input', 'value'),
+        Input('non_401k_investments_input', 'value'),
+        Input('existing_401k_input', 'value'),
+        Input('ongoing_401k_contribution_input', 'value'),
+        Input('employer_401k_contribution_pct_input', 'value'),
+        Input('employer_401k_contribution_rate_input', 'value'),
+    ]
+)
+def update_savings_graph(email, dob, dma, inc_pctile, salary, housing_exp,
+                         non_housing_exp, initial_savings, current_investments,
+                         existing_401k, ongoing_401k_contrib_pct,
+                         employer_401k_pct, employer_401k_rate):
+
+    port = create_portfolio(email, dob, dma, inc_pctile, salary, housing_exp,
+                            non_housing_exp, initial_savings,
+                            current_investments, existing_401k,
+                            ongoing_401k_contrib_pct/100,
+                            employer_401k_pct/100, employer_401k_rate/100)
+
+    # to_plt = port.assets.generate_path_df().reset_index().melt(id_vars='index')
+    # return px.line(to_plt, x='index', y='value', color='variable')
+    to_plt = port.assets.contents['401k'].simulate_path().reset_index()
+    return px.line(to_plt, x='index', y='total_value')
