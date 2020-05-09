af28ecf41d9ddcbe31bc41b1be4641b1e6a45cee
David Brodsky <davidbrodsky21@gmail.com>
2020-05-09 14:34:14 -0400

    Moving asset returns/growth logic to its own module. Implemented for Equities thus far.

--- gameplan/assets.py
+++ gameplan/assets.py
@@ -2,14 +2,15 @@ import numpy as np
 import pandas as pd
 import random
 
-import fecon236.prob.sim as fe_sim
-
 from gameplan.cashflows import CashFlow
 from gameplan.gp_collections import CashFlowCollection, Collection
 from gameplan.contributions import Contribution
+from gameplan.growth.asset_returns import AssetReturns
 
 
 class Asset():
+    RETURNS = AssetReturns()
+
     def __init__(self, asset_type, initial_balance, date_range):
         self.asset_type = asset_type
         self.initial_balance = initial_balance
@@ -33,9 +34,6 @@ class Asset():
     def add_debit(self, debit, label=None, if_exists='error'):
         label = label if label else debit.name
         self.credits_and_debits.add_object(debit, label, if_exists)
-
-    def simulate_path(self, **kwargs):
-        raise NotImplementedError
 
     def _get_compound_factors(self, date_diffs, *args, **kwargs):
         raise NotImplementedError
@@ -88,116 +86,33 @@ class CashSavings(Asset):
             )
 
 
-    def simulate_path(self):
-        """There's at least some stochasticity here around interest rate"""
-        return self.value_through_time
-
-
 class Equity(Asset): # should be type Investment
     DEFAULT_DATE_RANGE = pd.date_range(
         start=pd.datetime.today().date(),
         end=pd.datetime.today().date() + pd.DateOffset(years=20),
         freq='B'
     )
-    DEFAULT_MU = fe_sim.SPXmean
-    DEFAULT_SIGMA = fe_sim.SPXsigma
 
-    def __init__(self, ticker='SPY', init_value=1, mu=DEFAULT_MU,
-                 sigma=DEFAULT_SIGMA, date_range=DEFAULT_DATE_RANGE):
+    def __init__(self, ticker='SPY', init_value=1, date_range=DEFAULT_DATE_RANGE):
         super().__init__(
-            asset_type='Equity',
+            asset_type='equity',
             initial_balance=init_value,
             date_range = date_range
             )
-        self.ticker=ticker
-        self.init_value = init_value
-        self.mu = mu
-        self.sigma = sigma
-
-
-    def _generate_returns(self):
-        return fe_sim.gmix2ret(N=len(self.date_range), mean=self.mu, sigma=self.sigma)
-
+        self.returns_series = Asset.RETURNS.get_returns_series(
+            asset_type='equity',
+            ticker=ticker
+            )
 
     def _get_compound_factors(self, date_diffs):
-        return (pd.Series(
-                    index=self.date_range,
-                    data=self._generate_returns()
-                    ).shift(1)
+        returns_path = self.returns_series.returns_df['sim_1'] # just take 1st sim
+        return (returns_path
+                .shift(1)
                 .reindex(date_diffs.index)
                 .resample('D')
                 .first()
                 .fillna(1.0)
                 )
-
-
-    def _generate_price_path(self, label=None):
-        # Unclear that we need this plus simulate_path
-        return self.value_through_time
-
-
-    def simulate_path(self):
-        # Unclear that we need this plus _generate_price_path or the resample.pad()
-        return self._generate_price_path().resample('D').pad()
-
-
-    def _generate_price_paths(self, how_many, write_type=None):
-        """
-            write_type: str, default None
-                None - don't save paths to object
-                overwrite - overwrite whatever's in self._price_paths
-                append - add to whatever's in self._price_paths
-                error - raise ValueError when self._price_paths is not empty else write to it
-        """
-        paths = []
-        for x in range(how_many):
-            paths.append(self._generate_price_path())
-
-        # TO DO: This is probably unacceptably gross, so we should rewrite it
-        if write_type is None:
-            pass
-        elif write_type == 'overwrite':
-            self._price_paths = paths
-        elif write_type == 'append':
-            if hasattr(self, '_price_paths'):
-                self._price_paths.append(paths)
-            else:
-                self._price_paths = paths
-        elif write_type == 'error':
-            if hasattr(self, '_price_paths'):
-                raise ValueError("self._price_paths is not empty, if you want to overwrite or append, specify w/ write_type")
-            else:
-                self._price_paths = paths
-        else:
-            raise ValueError(f"{write_type} is not an acceptable value for write_type")
-
-        return paths
-
-
-    def plot_price_paths(self, max_plots=1, random_sample=True, **kwargs):
-        all_paths = (self._price_paths if hasattr(self, '_price_paths')
-                     else self._generate_price_paths(how_many=max_plots)
-                     )
-        paths_to_plt = (random.sample(all_paths, max_plots) if random_sample
-                        else all_paths[:max_plots]
-                        )
-        return pd.concat(paths_to_plt, axis=1).plot(**kwargs)
-
-
-    def plot_price_path_quantiles(self, min_n=200, quantiles=[0.25, 0.5, 0.75],
-                                  return_df=False, show_mean=False, **kwargs):
-        n_paths_to_gen = (min_n if not hasattr(self, '_price_paths')
-                          else min_n - len(self._price_paths)
-                          )
-        new_paths = self._generate_price_paths(how_many=n_paths_to_gen)
-        all_paths = (self._price_path.append(new_paths) if hasattr(self, '_price_paths')
-                     else new_paths
-                     )
-        all_paths_df = pd.concat(all_paths, axis=1)
-        quantiles = all_paths_df.quantile(q=quantiles, axis=1).T
-        if show_mean: quantiles['mean'] = all_paths_df.mean(axis=1)
-        quantiles.plot(**kwargs)
-        if return_df: return quantiles
 
 
 class Assets(Collection):
@@ -215,7 +130,7 @@ class Assets(Collection):
 
     def generate_path_df(self):
         df = pd.DataFrame(
-            { k: v.simulate_path() for k,v in self.contents.items() }
+            { k: v.value_through_time for k,v in self.contents.items() }
         ).fillna(method='bfill')
 
         df['total'] = df.sum(axis=1)
