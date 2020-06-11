485185dbeb02eed6d155a3104f3dfab814cc4b88
David Brodsky <davidbrodsky21@gmail.com>
2020-06-10 22:15:22 -0400

    playing around w/ memory error.

--- index.py
+++ index.py
@@ -15,14 +15,14 @@ app.layout = html.Div([
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/apps/salary-calculator':
-        return salary_calculator.layout
+    # if pathname == '/apps/salary-calculator':
+    #     return salary_calculator.layout
     if pathname == '/apps/income-forecast':
         return income_forecast.layout
     if pathname == '/apps/forms':
         return forms.layout
-    if pathname == '/apps/child_expenditures':
-        return child_expenditures.layout
+    # if pathname == '/apps/child-expenditures':
+    #     return child_expenditures.layout
     else:
         return '404'
 
